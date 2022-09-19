5097bb0546c42d17bba4ce92ad40a5e53fe3ab70
Im-Rises <quentin-morel88@hotmail.com>
2022-09-18 22:02:31 -0400

    Added some code example

--- PhysicalEngine/PhysicalEngine.cpp
+++ PhysicalEngine/PhysicalEngine.cpp
@@ -108,34 +108,7 @@ void PhysicalEngine::start() {
     ImVec4 clearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     drg::Triangle triangle;
-//    Shader shader("./shaders/triangleShader.vert", "./shaders/triangleShader.frag");
-//    float vertices[] = {
-//            // positions         // colors
-//            0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-//            -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-//            0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top
-//
-//    };
-//
-//    unsigned int VBO, VAO;
-//    glGenVertexArrays(1, &VAO);
-//    glGenBuffers(1, &VBO);
-//    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-//    glBindVertexArray(VAO);
-//
-//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-//
-//    // position attribute
-//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-//    glEnableVertexAttribArray(0);
-//    // color attribute
-//    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-//    glEnableVertexAttribArray(1);
-//
-//    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-//    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-//    // glBindVertexArray(0);
+//    drg::Square square;
 
     while (!glfwWindowShouldClose(window)) {
         /*------------------Pool events------------------*/
@@ -164,16 +137,11 @@ void PhysicalEngine::start() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         /*------------------Shader test------------------*/
-//        shader.use();
-//        glBindVertexArray(VAO);
-//        glDrawArrays(GL_TRIANGLES, 0, 3);
         triangle.use();
+//        square.use();
 
         /*------------------Swap buffers------------------*/
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
     }
-
-//    glDeleteVertexArrays(1, &VAO);
-//    glDeleteBuffers(1, &VBO);
 }
