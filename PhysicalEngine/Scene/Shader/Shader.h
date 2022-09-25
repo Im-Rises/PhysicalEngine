558ebb5df0754b1a6e92a7e8e4b7d2bbbce7aa0f
Im-Rises <quentin-morel88@hotmail.com>
2022-09-25 14:15:21 -0400

    wip

--- PhysicalEngine/Scene/Shader/Shader.h
+++ PhysicalEngine/Scene/Shader/Shader.h
@@ -12,6 +12,24 @@
 
 class Shader {
 private:
+    const char *vertexShader = "#version 330 core\n"
+                               "layout (location = 0) in vec3 aPos;\n"
+                               "\n"
+                               "uniform mat4 model;\n"
+                               "uniform mat4 view;\n"
+                               "uniform mat4 projection;\n"
+                               "\n"
+                               "void main()\n"
+                               "{\n"
+                               "    gl_Position = projection * view * model * vec4(aPos, 1.0f);\n"
+                               "}\0";
+    const char *fragmentShader = "#version 330 core\n"
+                                 "out vec4 FragColor;\n"
+                                 "\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "    FragColor = vec4(1.0f, 0.5f, 0.5f, 1.0f);\n"
+                                 "}\0";
     unsigned int ID;
 
 public:
