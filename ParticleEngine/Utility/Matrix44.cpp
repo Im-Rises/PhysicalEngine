131926faecbc238828205bca52629210b76dcbba
Im-Rises <quentin-morel88@hotmail.com>
2022-11-14 08:34:57 -0500

    wip

--- ParticleEngine/Utility/Matrix44.cpp
+++ ParticleEngine/Utility/Matrix44.cpp
@@ -21,7 +21,7 @@ float Matrix44::operator[](int i) const {
 }
 
 Matrix44 &Matrix44::operator*(const Matrix44 &matrix) {
-    Matrix44 res = Matrix44();
+    Matrix44 res;
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             for (int k = 0; k < 4; k++) {
