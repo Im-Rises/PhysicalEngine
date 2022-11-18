5a36cdfef96277d52219cdde27f2056a659deaed
spiryti <106757618+spiryti@users.noreply.github.com>
2022-11-18 00:35:13 -0500

    add *

--- ParticleEngine/Utility/Quaternion.cpp
+++ ParticleEngine/Utility/Quaternion.cpp
@@ -8,27 +8,18 @@ Quaternion::Quaternion()
 	m_value[3] = 0;
 }
 
-/// <summary>
-/// Constructeur de copy
-/// </summary>
-/// <param name="quaternion"></param>
+
 Quaternion::Quaternion(const Quaternion& quaternion)
 {
 	for (int i = 0; i < 4; i++) {
 		m_value[i] = quaternion.m_value[i];
 	}
 }
-/// <summary>
-/// Constructeur avec une vecteur : {0,Vector3d}
-/// </summary>
-/// <param name="vec"></param>
+
 Quaternion::Quaternion(const Vector3d& vec): Quaternion(0,vec.x,vec.y,vec.z)
 {
 }
-/// <summary>
-/// norm d'un quaternion
-/// </summary>
-/// <returns></returns>
+
 float Quaternion::norm()
 {
 	float sumSquar=0;
@@ -37,9 +28,7 @@ float Quaternion::norm()
 	}
 	return sqrt(sumSquar);
 }
-/// <summary>
-/// normalize le quaternion
-/// </summary>
+
 void Quaternion::normalize()
 {
 	float n = norm();
@@ -54,13 +43,7 @@ void Quaternion::normalize()
 	{
 	}
 }
-/// <summary>
-/// Constructeur avec les 4 composantes
-/// </summary>
-/// <param name="w"></param>
-/// <param name="x"></param>
-/// <param name="y"></param>
-/// <param name="z"></param>
+
 Quaternion::Quaternion(float w,float x, float y, float z)
 {
 	m_value[0] = w;
@@ -68,10 +51,7 @@ Quaternion::Quaternion(float w,float x, float y, float z)
 	m_value[2] = y;
 	m_value[3] = z;
 }
-/// <summary>
-/// Multiplication de quaternion selon le produit hamiltonien
-/// <param name="quaternion"></param>
-/// <returns></returns>
+
 Quaternion&  Quaternion::operator*=(const Quaternion& quaternion)
 {
 	float w, x, y, z;
@@ -89,72 +69,45 @@ Quaternion&  Quaternion::operator*=(const Quaternion& quaternion)
 }
 
 
-/// <summary>
-/// Multiplication de quaternion selon le produit hamiltonien methode constante
-/// </summary>
-/// <param name="quaternion"></param>
-/// <returns></returns>
+
 Quaternion Quaternion::operator*(const Quaternion& quaternion)
 {
 	Quaternion res(*this);
 	res *= quaternion;
 	return res;
 }
-/// <summary>
-/// mutliplication par un float
-/// </summary>
-/// <param name="n"></param>
-/// <returns></returns>
+
 Quaternion Quaternion::operator*(float n)
 {
 	return Quaternion(n * m_value[0], n * m_value[1], n * m_value[2], n * m_value[3]);
 }
 
-/// <summary>
-/// somme de quaternion
-/// </summary>
-/// <param name="quaternion"></param>
-/// <returns></returns>
+
 Quaternion Quaternion::operator+(const Quaternion& quaternion)
 {
 	return Quaternion(m_value[0] + quaternion.m_value[0], m_value[1] + quaternion.m_value[1],
 		m_value[2] + quaternion.m_value[2], m_value[3] + quaternion.m_value[3]);
 }
-/// <summary>
-/// Geteur des composantes de quaternion
-/// </summary>
-/// <param name="i"></param>
-/// <returns></returns>
+
 float Quaternion::operator[](int i) const
 {
 	return m_value[i];
 }
-/// <summary>
-/// Seteur d'une composante de queternion
-/// </summary>
-/// <param name="i"></param>
+
 void Quaternion::set(int i)
 {
 	m_value[i] = i;
 	normalize();
 }
 
-/// <summary>
-/// Effectue la rotation d'un quaternion en suivant un vecteur de rotation
-/// </summary>
-/// <param name="vector"></param>
+
 void Quaternion::RotateByVector(const Vector3d& vector)
 {
 	*this *= Quaternion(vector);
 }
 
 
-/// <summary>
-/// Effectue la rotation du quaternion selon les vitesse angulaire correspondant au 3 degrés de liberté définit dans le Vectord3D 
-/// pendant un temps time
-/// </summary>
-/// <param name="vector"></param>
-/// <param name="time"></param>
+
 void Quaternion::UpdateByAngularSpeed(const Vector3d& vector, float time)
 {
 	Quaternion updateQuaternion = *this + Quaternion(vector) * *this * (time / 2);
--- test/quaternionTest.cpp
+++ test/quaternionTest.cpp
@@ -54,18 +54,80 @@ int normalizeTest() {
 
 
 int multTest() {
+    Quaternion q0 = Quaternion(1, 1, 1, 1);
+    q0.normalize();
+    Quaternion q1 = Quaternion(1, 0, 0, 0);
+    Quaternion q2 = Quaternion(0, 1, 0, 0);
+    Quaternion q3 = Quaternion(0, 0, 1, 0);
+    Quaternion q4 = Quaternion(0, 0, 0, 1);
+    
+    Quaternion qt1 = q0 * q1;
+    Quaternion qt2 = q0 * q2;
+    Quaternion qt3 = q3 * q0;
+    Quaternion qt4 = q4 * q0;
+    if (!(qt1[0] == 0.5f && qt1[1] == 0.5f && qt1[2] == 0.5f && qt1[3] == 0.5f))
+    {
+        std::cout << "- multiplication fail 1 1 1 1 * 1 0 0 0 !\n";
+        return 1;
+    }
+    if (!(qt2[0] == -0.5f && qt2[1] == 0.5f && qt2[2] == 0.5f && qt2[3] == -0.5f))
+    {
+        std::cout << "- multiplication fail 1 1 1 1 * 0 1 0 0 !\n";
+        return 1;
+    }
+    if (!(qt3[0] == -0.5f && qt3[1] == 0.5f && qt3[2] == 0.5f && qt3[3] == -0.5f))
+    {
+        std::cout << "- multiplication fail 0 0 1 0 * 1 1 1 1  !\n";
+        return 1;
+    }
+    if (!(qt4[0] == -0.5f && qt4[1] == -0.5f && qt4[2] == 0.5f && qt4[3] == 0.5f))
+    {
+        std::cout << "- multiplication fail 0 0 0 1 * 1 1 1 1 !\n";
+        return 1;
+    }
     return 0;
+
 }
 
-int plusTest() {
-    return 0;
-}
+
 int rotateByVectorTest() {
+    Quaternion qt2 = Quaternion(1, 1, 1, 1);
+    Quaternion qt3 = Quaternion(1, 1, 1, 1);
+    Quaternion qt4 = Quaternion(1, 1, 1, 1);
+    qt2.normalize();
+    qt3.normalize();
+    qt4.normalize();
+    qt2.RotateByVector(Vector3d(1,0,0));
+    qt3.RotateByVector(Vector3d(0, 1, 0));
+    qt4.RotateByVector(Vector3d(0, 0, 1));
+    if (!(qt2[0] == -0.5f && qt2[1] == 0.5f && qt2[2] == 0.5f && qt2[3] == -0.5f))
+    {
+        std::cout << "- rotation fail 1 1 1 1 * 1 0 0 !\n";
+        return 1;
+    }
+
+    if (!(qt3[0] == -0.5f && qt3[1] == -0.5f && qt3[2] == 0.5f && qt3[3] == 0.5f))
+    {
+        std::cout << "- rotation fail 1 1 1 1 * 0 1 0  !\n";
+        return 1;
+    }
+    if (!(qt4[0] == -0.5f && qt4[1] == 0.5f && qt4[2] == -0.5f && qt4[3] == 0.5f))
+    {
+        std::cout << "- rotation fail fail 1 1 1 1 * 0 0 1 !\n";
+        return 1;
+    }
     return 0;
 }
 
 
 int UpdateByAngularSpeedTest() {
+    Quaternion q0 = Quaternion(1, 2, 3, 4);
+    q0.UpdateByAngularSpeed(Vector3d(1, 0, 0), 1);
+    if (!(q0[0] == 0.0f && q0[1] == 2.5f && q0[2] == 1.0f && q0[3] == 5.5f))
+    {
+        std::cout << "- rotatebyangularspeed fail 1 2 3 4 rotation speed: 1 0 0 pendant une seconde !\n";
+        return 1;
+    }
     return 0;
 }
 
@@ -78,7 +140,6 @@ int main() {
     result = +normTest();
     result += normalizeTest();
     result += multTest();
-    result += plusTest();
     result += rotateByVectorTest();
     result += UpdateByAngularSpeedTest();
     if (result == 0)
