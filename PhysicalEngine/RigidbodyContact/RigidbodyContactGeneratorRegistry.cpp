d3765f7bf9d572657dd87b1d0f1d84791b091c67
Im-Rises <quentin-morel88@hotmail.com>
2022-12-06 22:00:48 -0500

    Added sphere to sphere contact

--- PhysicalEngine/RigidbodyContact/RigidbodyContactGeneratorRegistry.cpp
+++ PhysicalEngine/RigidbodyContact/RigidbodyContactGeneratorRegistry.cpp
@@ -12,15 +12,17 @@ void RigidbodyContactGeneratorRegistry::calculateContact(RigidbodySphereCollider
     Vector3d position1 = rigidbodySphereCollider1->getGameObject()->transform.getPosition();
     Vector3d position2 = rigidbodySphereCollider2->getGameObject()->transform.getPosition();
 
-    if (pow(position1.distance(position2), 2) >= (rigidbodySphereCollider1->getRadius() + rigidbodySphereCollider2->getRadius()))
-    {
-        // Todo: Create contact
-        //  COLLISION
-        //        Rigidbody* rigidbody1 = rigidbodySphereCollider1->getGameObject()->getComponent<Rigidbody>();
-        //        Rigidbody* rigidbody2 = rigidbodySphereCollider2->getGameObject()->getComponent<Rigidbody>();
-
-        //        RigidbodyContact* rigidbodyContact = new RigidbodyContact(rigidbody1, rigidbody2, 0, 0, Vector3d(0, 0, 0));
-        //        rigidbodyContact->resolveContact();
+    if (pow(position1.distance(position2), 2) < (rigidbodySphereCollider1->getRadius() + rigidbodySphereCollider2->getRadius()))
         return;
-    }
+
+    std::cout << "Sphere to sphere contact between " << rigidbodySphereCollider1->getGameObject()->getName() << " and " << rigidbodySphereCollider2->getGameObject()->getName() << std::endl;
+
+    // Todo: Create contact
+    //  COLLISION
+
+    //        Rigidbody* rigidbody1 = rigidbodySphereCollider1->getGameObject()->getComponent<Rigidbody>();
+    //        Rigidbody* rigidbody2 = rigidbodySphereCollider2->getGameObject()->getComponent<Rigidbody>();
+
+    //        RigidbodyContact* rigidbodyContact = new RigidbodyContact(rigidbody1, rigidbody2, 0, 0, Vector3d(0, 0, 0));
+    //        rigidbodyContact->resolveContact();
 }
